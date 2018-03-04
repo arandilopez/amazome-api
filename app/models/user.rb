0189d421bcba8a027bd8a73b62721e18187b8fca
Arandi Lopez <arandilopez.93@gmail.com>
2018-03-03 18:43:31 -0600

    change order in user model config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   include ActiveModel::Validations
+  has_secure_password
   validates :email, uniqueness: true, presence: true, email: true
   validates_presence_of :name
   enum role: [:user, :admin]
-  has_secure_password
 end
