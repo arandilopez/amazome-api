47f596c5638b2ffc4c0b09ae853ae8f190ba3b34
Arandi Lopez <arandilopez.93@gmail.com>
2018-03-04 13:30:05 -0600

    Cart relations to users and products

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true, numericality: true
   has_and_belongs_to_many :wish_lists
+  has_and_belongs_to_many :carts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true, email: true
   validates_presence_of :name
   has_many :wish_lists
+  has_one :cart
   enum role: [:user, :admin]
 end
