d6b2a6934a4476db6af97b185e8ddec8e3a1f4d1
Arandi Lopez <arandilopez.93@gmail.com>
2018-03-03 22:13:14 -0600

    Wishlists and their relations to users and products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   validates :sku, presence: true, uniqueness: true
   validates :name, presence: true
   validates :price, presence: true, numericality: true
+  has_and_belongs_to_many :wish_lists
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: true, presence: true, email: true
   validates_presence_of :name
+  has_many :wish_lists
   enum role: [:user, :admin]
 end
