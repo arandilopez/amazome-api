50dd855b0949f85959c508698a5ed628d372206c
Arandi Lopez <arandilopez.93@gmail.com>
2018-03-03 22:14:12 -0600

    Fix users controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,34 +1,58 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin = create(:admin)
+    @users = create_list(:user, 10)
+    @user = create(:user)
+  end
+
   test "should get index" do
-    get users_index_url
+    get users_url, as: :json, headers: authenticated_header(@admin)
     assert_response :success
+    assert_equal 12, json_response.size
   end
 
-  test "should get create" do
-    get users_create_url
-    assert_response :success
+  test "should create new user" do
+    assert_difference("User.count", 1) do
+      post users_url, params: {
+        user: {name: "Fulano", email: "fulano@example.com", password: "fulano123", password_confirmation: "fulano123"}
+        }, as: :json, headers: authenticated_header(@admin)
+    end
+    assert_response :created
+    @user = User.last
+    assert_equal "Fulano", @user.name
   end
 
-  test "should get show" do
-    get users_show_url
+  test "should get user" do
+    get user_url(@user), as: :json, headers: authenticated_header(@admin)
     assert_response :success
+    assert_not json_response.include? 'password_digest'
   end
 
-  test "should get update" do
-    get users_update_url
+  test "should update user info" do
+    assert_no_difference("User.count") do
+      patch user_url(@user), params: {
+        user: {name: "Fulano", email: "fulano@example.com"}
+        }, as: :json, headers: authenticated_header(@admin)
+    end
     assert_response :success
+    @user.reload
+    assert_equal "Fulano", @user.name
   end
 
-  test "should get delete" do
-    get users_delete_url
-    assert_response :success
+  test "should delete user" do
+    assert_difference('User.count', -1) do
+      delete user_url(@user), as: :json, headers: authenticated_header(@admin)
+    end
+
+    assert_response :no_content
   end
 
-  test "should get current" do
-    get users_current_url
+  test "should get current user" do
+    get current_users_url, as: :json, headers: authenticated_header(@user)
     assert_response :success
+    assert_equal @user.id, json_response['id']
   end
 
 end
