3cb38e3a9c27c5f5d898e5f5a05cb3b340b1336b
Arandi Lopez <arandilopez.93@gmail.com>
2018-03-05 22:15:11 -0600

    User always need a cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
   include ActiveModel::Validations
-  has_secure_password
-  validates :email, uniqueness: true, presence: true, email: true
   validates_presence_of :name
+  validates :email, uniqueness: true, presence: true, email: true
+  has_secure_password
+  has_one :cart, dependent: :destroy
   has_many :wish_lists
-  has_one :cart
-  enum role: [:user, :admin]
+  enum role: %i{user admin}
+
+  after_create do
+    self.create_cart if self.cart.blank?
+  end
 end
