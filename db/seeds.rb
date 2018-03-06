970b094c24da50fa0bbf17755362633803c19e5f
Arandi Lopez <arandilopez.93@gmail.com>
2018-03-05 22:15:34 -0600

    creating seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+if User.count.zero?
+  User.create(name: "Arandi Lopez", email: "arandilopez.93@gmail.com", password: "username123", password_confirmation: "username123", role: :admin)
+
+  User.create((0..10).to_a.map {
+    {name: Faker::Name.name, email: Faker::Internet.unique.safe_email, password: "username123", password_confirmation: "username123"}
+    }).each { |user| user.wish_lists.create(name: Faker::Commerce.department(2)) }
+end
+
+if Product.count.zero?
+  Product.create((0..1000).to_a.map {
+    {sku: Faker::Code.unique.isbn, name: Faker::Commerce.product_name, price: Faker::Commerce.price, description: Faker::Commerce.material}
+  })
+end
