79b7ab42af51d58e5523528e142cbbc38bd14abb
Arandi Lopez <arandilopez.93@gmail.com>
2018-03-10 16:40:23 -0600

    Add image to product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304054527) do
+ActiveRecord::Schema.define(version: 20180310200708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180304054527) do
     t.decimal "price", precision: 20, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "products_wish_lists", id: false, force: :cascade do |t|
--- test/factories/products.rb
+++ test/factories/products.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     name { Faker::Commerce.product_name }
     description { Faker::Commerce.department }
     price { Faker::Commerce.price }
+    image { Faker::LoremPixel.image("512x512") }
   end
 end
