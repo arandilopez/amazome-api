8b560e16cde54e4909821d0d00b211cf7b33f65a
Arandi Lopez <arandilopez.93@gmail.com>
2018-03-10 09:51:55 -0600

    cors config

--- app/models/wish_list.rb
+++ app/models/wish_list.rb
@@ -1,4 +1,5 @@
 class WishList < ApplicationRecord
+  validates_presence_of :name
   belongs_to :user
   has_and_belongs_to_many :products
 end
--- config/application.rb
+++ config/application.rb
@@ -29,12 +29,6 @@ module Amazome
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :patch ,:delete]
-      end
-    end
     config.timezone = 'America/Merida'
   end
 end
